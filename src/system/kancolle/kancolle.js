759d9f6e5a66aed3b29e9089bce00f8b4212b42b
dragonjet <jetriconew@gmail.com>
2017-07-12 08:42:24 +0800

    scrap experiments

--- src/system/kancolle/kancolle.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(){
-  return {
-    api: require('api/kancolle-api.js'),
-    data: require('data/kancolle-data.js'),
-    master: require('master/kancolle-master.js'),
-    mechanics: require('mechanics/kancolle-mechanics.js')
-  };
-}();
