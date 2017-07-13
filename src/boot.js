c5d40eeb6fdd183b6edacac619a5a330fb19419d
FOG_Yamato <contact@cybersnets.com>
2017-07-13 03:25:30 +0300

    Added lock file Added icon Made window size and position persistent

--- src/boot.js
+++ src/boot.js
@@ -1,25 +1,37 @@
 import path from 'path'
 import url from 'url'
 import electron from 'electron'
+import windowState from 'electron-window-state'
 
 const app = electron.app
 
 // Application ready state
 app.on('ready', () => {
 
-  let mainWindow = new electron.BrowserWindow({
-    width: 900,
-    height: 800,
-    useContentSize: true,
-    autoHideMenuBar: true
+  let winState = windowState({
+    defaultWidth: 900,
+    defaultHeight: 800,
   })
+
+  let mainWindow = new electron.BrowserWindow({
+    width: winState.width,
+    height: winState.height,
+    x: winState.x,
+    y: winState.y,
+    useContentSize: true,
+    autoHideMenuBar: true,
+    icon: __dirname + "/browser/assets/images/favicon.ico",
+  })
+
+  winState.manage(mainWindow)
+
   mainWindow.setMenu(null)
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'browser/html/main.html'),
     protocol: 'file:',
     slashes: true
   }))
-  mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
 
 })
 
