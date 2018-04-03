fbce46918ece8c9f475dc58324e16f5b779aa2c9
Jet Rico <jetriconew@gmail.com>
2018-04-03 17:15:54 +0800

    Yarn + Electron build

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 import { app, BrowserWindow } from 'electron'
 import * as path from 'path'
 import { format as formatUrl } from 'url'
+import 'source-map-support'
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
